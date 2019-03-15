6cb461f1bb16daf20b1be5881803abe9ae428d71
lokha <aleksey.kovalchuk98@gmail.com>
2019-03-15 22:42:52 +0200

    Сделал глобальные переменные

--- src/net/deathcoder/coding/DeathHelper.java
+++ src/net/deathcoder/coding/DeathHelper.java
@@ -1,15 +1,16 @@
 package net.deathcoder.coding;
 
+import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.Material;
-import org.bukkit.block.Block;
-import org.bukkit.block.BlockFace;
-import org.bukkit.block.Sign;
+import org.bukkit.World;
+import org.bukkit.block.*;
 import org.bukkit.event.block.Action;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.ArrayList;
-import java.util.Objects;
+import java.util.*;
 
 public class DeathHelper {
     public static final boolean isRightClick(final PlayerInteractEvent $receiver) {
@@ -56,13 +57,74 @@ public class DeathHelper {
         return null;
     }
 
-    public static void runSign(final Sign sign) {
-        final String text = sign.getLine(1);
-        final Location loc = sign.getLocation();
-
-        if (text.equalsIgnoreCase("Молния")) {
-            loc.getWorld().strikeLightningEffect(loc);
+    public static void runSign(Sign sign, Map<String, Object> globalVars) {
+        if (sign.getLine(0).equalsIgnoreCase("[event]")) {
             return;
         }
+
+        String text = sign.getLine(1);
+        Location signLoc = sign.getLocation();
+        List<Object> localVars = getVarsFromSign(sign);
+
+        if (text.equalsIgnoreCase("Молния")) {
+            for (Object var : localVars) {
+                if (var instanceof Location) {
+                    Location loc = (Location) var;
+                    loc.getWorld().strikeLightning(loc);
+                }
+            }
+        }
+
+        if (text.equalsIgnoreCase("Ломать дважды")) {
+            Block block = (Block) globalVars.get("block-break");
+            block.getRelative(BlockFace.DOWN).setType(Material.AIR);
+        }
+    }
+
+    public static List<Object> getVarsFromSign(Sign sign) {
+        Block signBlock = sign.getBlock();
+        byte data = signBlock.getData();
+
+        BlockFace blockFace = null;
+        if (data == 3) {
+            blockFace = BlockFace.NORTH;
+        } else if (data == 4) {
+            blockFace = BlockFace.EAST;
+        } else if (data == 2) {
+            blockFace = BlockFace.SOUTH;
+        } else if (data == 5) {
+            blockFace = BlockFace.WEST;
+        }
+
+        Block wallBlock = signBlock.getRelative(blockFace);
+        Block chectBlock = wallBlock.getRelative(BlockFace.UP);
+
+        if (!chectBlock.getType().equals(Material.CHEST)) {
+            return new ArrayList<>();
+        }
+
+        Chest chest = (Chest) chectBlock.getState();
+
+        List<Object> vars = new ArrayList<>();
+        for (ItemStack item : chest.getBlockInventory().getContents()) {
+            if (item != null) {
+                ItemMeta itemMeta = item.getItemMeta();
+                String textData = itemMeta.getLocalizedName();
+
+                if (textData.startsWith("var-location")) {
+                    String[] split = textData.split(":");
+                    String worldName = split[1];
+                    Integer x = Integer.parseInt(split[2]);
+                    Integer y = Integer.parseInt(split[3]);
+                    Integer z = Integer.parseInt(split[4]);
+                    World world = Bukkit.getWorld(worldName);
+
+                    Location location = new Location(world, x, y, z);
+                    vars.add(location);
+                }
+            }
+        }
+
+        return vars;
     }
 }
--- src/net/deathcoder/coding/listener/PlayerListener.java
+++ src/net/deathcoder/coding/listener/PlayerListener.java
@@ -8,60 +8,91 @@ import org.bukkit.block.Block;
 import org.bukkit.block.Sign;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
 import org.bukkit.event.player.PlayerInteractEvent;
+import org.bukkit.inventory.EquipmentSlot;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.meta.ItemMeta;
 
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
+import java.util.*;
 
 import static net.deathcoder.coding.DeathHelper.*;
 
 public class PlayerListener implements Listener {
     private final DeathCodingPlugin plugin;
-
-    @EventHandler
-    public void onInteract(final PlayerInteractEvent event) {
-        if (!isRightClick(event)) return;
-        if (event.getClickedBlock() == null || event.getClickedBlock().getType() != Material.WALL_SIGN) return;
-
-        Player player = event.getPlayer();
-        Block block = event.getClickedBlock();
-        Sign signBlock = (Sign) block.getState();
-        Location blockLoc = signBlock.getLocation();
-
-        do {
-            runSign(signBlock);
-            player.sendMessage("нашёл!");
-        } while ((signBlock = getNextSign(signBlock)) != null);
-    }
+    private Set<Sign> onEvents = new HashSet<>();
 
     public PlayerListener(final DeathCodingPlugin plugin) {
         this.plugin = plugin;
     }
 
-    public static class Key {
-        private final int i;
-
-        public Key(int i) {
-            this.i = i;
+    @EventHandler
+    public void onInteract(final PlayerInteractEvent event) {
+        if (event.getClickedBlock() != null && event.getClickedBlock().getType() ==  Material.WALL_SIGN) {
+            Player player = event.getPlayer();
+            Block block = event.getClickedBlock();
+            Sign signBlock = (Sign) block.getState();
+            this.runCode(player, signBlock, new HashMap<>());
+        } else if (event.getHand().equals(EquipmentSlot.HAND)
+                && event.getClickedBlock() != null
+                && event.getClickedBlock().getType() != Material.CHEST
+                && event.getItem() != null
+                && event.getItem().getType() == Material.SLIME_BALL) {
+            this.setLocationItem(event);
         }
+    }
 
-        public int getI() {
-            return i;
-        }
 
-        @Override
-        public boolean equals(Object o) {
-            if (this == o) return true;
-            if (o == null || getClass() != o.getClass()) return false;
-            Key key = (Key) o;
-            return i == key.i;
-        }
+    @EventHandler
+    public void on(BlockBreakEvent event) {
+        for (Sign sign : onEvents) {
+            if (sign.getLine(1).equalsIgnoreCase("ломать блок")) {
 
-        @Override
-        public int hashCode() {
-            return i;
+                HashMap<String, Object> vars = new HashMap<>();
+                vars.put("block-break", event.getBlock());
+                vars.put("player-break", event.getPlayer());
+
+                runCode(sign, vars);
+            }
         }
+    }
+
+    private void runCode(Player player, Sign sign, Map<String, Object> globalVars) {
+        if (sign.getLine(0).equalsIgnoreCase("[event]")) {
+            onEvents.add(sign);
+            player.sendMessage("добавили табличку в event'ы");
+        } else {
+            runCode(sign, globalVars);
+        }
+    }
+
+    private void runCode(Sign sign, Map<String, Object> globalVars) {
+        do {
+            runSign(sign, globalVars);
+        } while ((sign = getNextSign(sign)) != null);
+    }
+
+    private void setLocationItem(PlayerInteractEvent event) {
+        ItemStack item = event.getItem();
+        Block clickedBlock = event.getClickedBlock();
+
+        ItemMeta itemMeta = item.getItemMeta();
+        itemMeta.setLocalizedName("var-location:" +
+                clickedBlock.getWorld().getName() +
+                ":" + clickedBlock.getX() +
+                ":" + clickedBlock.getY() +
+                ":" + clickedBlock.getZ());
+
+        itemMeta.setDisplayName("Локация: " +
+                clickedBlock.getWorld().getName() +
+                " " + clickedBlock.getX() +
+                "," + clickedBlock.getY() +
+                "," + clickedBlock.getZ());
+        item.setItemMeta(itemMeta);
+        event.getPlayer().getInventory().setItemInMainHand(item);
+
+        event.getPlayer().sendMessage("Локация установлена!");
     }
 }
